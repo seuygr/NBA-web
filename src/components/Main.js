34ffcfa55504cfa613e429ff044d3d6e1a6601db
Guorui Yang <32991261+seuygr@users.noreply.github.com>
2018-10-17 18:50:17 -0700

    10.17

---
+++ src/components/Main.js
@@ -0,0 +1,41 @@
+import React from 'react';
+import {Profile} from "./Profile"
+import nba from 'nba';
+import { ShotChart} from "./ShotChart"
+
+window.nba = nba;
+
+export class Main extends React.Component {
+    state = {
+        playerInfo: {
+            playerId: 201939
+        }
+    }
+
+    componentDidMount() {
+        const playerId = nba.findPlayer('Stephen Curry').playerId;
+
+        nba.stats.playerInfo({PlayerID: 201939}).then(
+            (info) =>{
+                const playerInfo = {
+                    ...info.commonPlayerInfo[0],
+                    ...info.playerHeadlineStats[0]
+                };
+
+                this.setState({
+                   playerInfo
+                });
+            }
+        );
+    }
+
+
+    render() {
+        return (
+            <div className="main">
+                <Profile playerInfo={this.state.playerInfo} />
+                <ShotChart playerId={this.state.playerInfo.playerId} />
+            </div>
+        );
+    }
+}
